5bad3138a465012b3ef397e7962227e0126f3a6a
Antoine Groudiev <antoine.groudiev@gmail.com>
2022-04-14 22:38:03 +0200

    Ajout des WeakClassifiers

---
+++ source/python_impl_FS/WeakClassifier.py
@@ -0,0 +1,12 @@
+from utilitaires import evaluation
+
+class WeakClassifier:
+    def __init__(self, positive_regions: list, negative_regions: list, treshold: float, polarity: int):
+        assert(polarity == -1 or polarity == 1)
+        self.positive_regions = positive_regions
+        self.negative_regions = negative_regions
+        self.treshold = treshold
+        self.polarity = polarity
+
+    def classify(self, ii: list):
+        return 1 if self.polarity * evaluation(ii) < self.polarity * self.treshold else 0
