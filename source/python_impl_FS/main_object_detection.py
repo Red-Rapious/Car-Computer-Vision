7152005cfdcdc2cbb7c9f8d933c2e352b2517487
Antoine Groudiev <antoine.groudiev@gmail.com>
2022-04-21 12:15:40 +0200

    Changement de nom et ajout d'images de test

---
+++ source/python_impl_FS/main_object_detection.py
@@ -0,0 +1,9 @@
+"""
+| Créé par Antoine Groudiev - 2022
+| - Implémentation des travaux de Paul Viola et Michael Jones
+| - Sur un tutoriel original de Anmol Parande 
+| - https://medium.datadriveninvestor.com/understanding-and-implementing-the-viola-jones-image-classification-algorithm-85621f7fe20b
+"""
+
+if __name__ == "__main__":
+    pass
