f1839e5f3da4a2d24a822e5bab1b51d35221f0bd
Red-Rapious <red.rapious@gmail.com>
2022-07-23 23:43:13 +0300

    Avancée du programme de data processing

--- source/training_data_processing/data_processing.py
+++ source/training_data_processing/data_processing.py
@@ -1,14 +1,21 @@
 import cv2
 import numpy as np
 
-def format_image(image_path: str):
-    image = cv2.imread(image_path, 1)
+UNPROCESSED_FOLDER = "/Users/antoinegroudiev/Documents/Code/Car-Computer-Vision/ressources/training_images/stop_signs_images_unprocessed/"
+PROCESSED_FOLDER = "/Users/antoinegroudiev/Documents/Code/Car-Computer-Vision/ressources/training_images/stop_signs_images_processed/"
+
+def format_image(image_name: str, type="train"):
+    image = cv2.imread(UNPROCESSED_FOLDER + type + "/" + image_name, 1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.resize(image, (19, 19), interpolation = cv2.INTER_LINEAR)
 
-    show_image = cv2.resize(image, (190, 190), interpolation=cv2.INTER_NEAREST)
-    cv2.imshow("Image", show_image)
-    cv2.waitKey(0)
+    #show_image = cv2.resize(image, (190, 190), interpolation=cv2.INTER_NEAREST)
+    #cv2.imshow("Image", show_image)
+    #cv2.waitKey(0)
+    return image
+
+def save_image(image, image_name: str, type="train"):
+    cv2.imwrite(PROCESSED_FOLDER + type + "/" + image_name, image)
 
 if __name__ == "__main__":
-    format_image("/Users/antoinegroudiev/Documents/Code/Car-Computer-Vision/ressources/training_images/stop_signs_images_unprocessed/data/test/stop_sign/gray-01MB90O9N0J2.jpg")
+    format_image("0G8PNL4D4CI0.jpg", "train")
