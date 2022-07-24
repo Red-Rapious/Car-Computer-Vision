bd2d4f8a7d06e95f426b53cbdcbb9b195ff4017e
Red-Rapious <red.rapious@gmail.com>
2022-07-24 15:11:59 +0300

    Fin du programme de processing

--- source/training_data_processing/data_processing.py
+++ source/training_data_processing/data_processing.py
@@ -6,17 +6,28 @@ UNPROCESSED_FOLDER = "/Users/antoinegroudiev/Documents/Code/Car-Computer-Vision/
 PROCESSED_FOLDER = "/Users/antoinegroudiev/Documents/Code/Car-Computer-Vision/ressources/training_images/stop_signs_images_processed/"
 
 def format_image(image_name: str, type="train"):
-    image = cv2.imread(UNPROCESSED_FOLDER + type + "/" + image_name, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.resize(image, (19, 19), interpolation = cv2.INTER_LINEAR)
-
-    #show_image = cv2.resize(image, (190, 190), interpolation=cv2.INTER_NEAREST)
-    #cv2.imshow("Image", show_image)
-    #cv2.waitKey(0)
-    return image
+    image = cv2.imread(UNPROCESSED_FOLDER + type + "/" + image_name, cv2.IMREAD_COLOR)
+    if image is not None:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (19, 19), interpolation = cv2.INTER_LINEAR)
+        return image
+    else:
+        print("[ERREUR] L'image '" + image_name +"' n'a pas pu être lue...")
+        exit(-1)
 
 def save_image(image, image_name: str, type="train"):
-    cv2.imwrite(PROCESSED_FOLDER + type + "/" + image_name, image)
+    result = cv2.imwrite(PROCESSED_FOLDER + type + "/" + image_name, image)
+    if not result:
+        print("[ERREUR] L'image '", image_name, "' n'a pas pu être enregistrée.")
+
+def process_subfolder(type="train"):
+    i = 0
+    for _, _, files in os.walk(UNPROCESSED_FOLDER + type):
+        for file in files:
+            if len(file) != 0 and file[0] != ".":
+                image = format_image(file, type)
+                save_image(image, "stop_sign_" + type + "_" + str(i) + ".pgm", type)
+                i += 1
 
 def process_entire_folder():
     print("Démarrage du processing des images")
@@ -24,20 +35,10 @@ def process_entire_folder():
     print("[INFO] Dossier sortie :", PROCESSED_FOLDER)
 
     print("Processing en cours des images d'entraînement...")
-    i = 0
-    for _, _, files in os.walk(UNPROCESSED_FOLDER + "train"):
-        for file in files:
-            image = format_image(file, "train")
-            save_image(image, "stop_sign_train_" + str(i), "train")
-            i += 1
+    process_subfolder("train")
     
     print("Processing en cours des images de test...")
-    i = 0
-    for _, _, files in os.walk(UNPROCESSED_FOLDER + "test"):
-        for file in files:
-            image = format_image(file, "test")
-            save_image(image, "stop_sign_test_" + str(i), "test")
-            i += 1
+    process_subfolder("test")
 
 
 if __name__ == "__main__":
