9c636b2f99ab1b829d41eab76112f0969e5c0e63
Red-Rapious <red.rapious@gmail.com>
2022-09-19 17:28:29 +0200

    Ajout des données du raspberry pi

--- source/stress_test/graphe_stress_test.py
+++ source/stress_test/graphe_stress_test.py
@@ -5,7 +5,7 @@ resultats = {
     "MacBook Pro": [108.2, 2.43, 7.69, 25.05, 39.7, 33.33],
     "iPad Pro": [142.12, 3.02, 8.88, 29.62, 51.81, 48.79],
     "iPhone XR": [222.53, 2.92, 10.02, 35.57, 78.95, 95.06],
-    "Raspberry Pi 4": [10, 10, 10, 10, 10, 10], # TODO: remplacer par des valeurs réelles
+    "Raspberry Pi 4": [559.23, 17.74, 43.18, 120.95, 192.02, 185.34],
 }
 
 # set width of bar
@@ -24,11 +24,10 @@ br2 = [x + barWidth for x in br1]
 br3 = [x + barWidth for x in br2]
 br4 = [x + barWidth for x in br3]
 
-# TODO: changer les couleurs des barres
-plt.bar(br1, MB, color ='r', width = barWidth, label ='MacBook Pro (Intel i7)')
+plt.bar(br1, MB, color = 'b', width = barWidth, label ='MacBook Pro (Intel i7)')
 plt.bar(br2, IPA, color ='g', width = barWidth, label ='iPad Pro (Apple A10X)')
-plt.bar(br3, IPO, color ='b', width = barWidth, label ='iPhone XR (Apple A12)')
-plt.bar(br4, RPI, color ='y', width = barWidth, label ='Raspberry Pi 4 (ARM Cortex A72)')
+plt.bar(br3, IPO, color ='y', width = barWidth, label ='iPhone XR (Apple A12)')
+plt.bar(br4, RPI, color ='r', width = barWidth, label ='Raspberry Pi 4 (ARM Cortex A72)')
  
 plt.xlabel("Le plus bas est le mieux", fontweight="bold")
 plt.ylabel("Temps d'exécution (secondes)", fontweight="bold")
